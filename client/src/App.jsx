5188ae1304cf4ad57a81953f40effd0f17e9b245
pradeep-2005 <22cs091@kcgcollege.com>
2025-07-25 14:40:22 +0530

    setup:tailwindcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-       <div className='w-8 h-8 text-amber-300 bg-amber-600'>hello</div>
+       <div className='w-8 h-8 text-amber-300 bg-amber-600'>
+
+        
+       </div>
         
     </>
   )
