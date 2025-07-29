c285e1040ca988c2e5017e6334ad7a67ad27c6ae
R-pradeep2005 <pradeepravisankar1@gmail.com>
2025-07-29 20:16:17 +0530

    Backend:mongodb connection and .env setup completed

--- server/index.js
+++ server/index.js
@@ -1,16 +1,29 @@
-const express =require('express');
-const app =express();
-const LoginRouter=require('./routes/login')
-const StudentRouter=require('./routes/student')
-const TeacherRouter=require('./routes/Teacher')
-const AddStudentRouter=require('./routes/AddStudent')
-const cors=require('cors')
-app.use(cors())
-app.use(express.json())
-app.use('/',LoginRouter);
-app.use('/Student',StudentRouter);
-app.use('/Teacher',TeacherRouter);
-app.use('/AddStudent',AddStudentRouter);
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const LoginRouter = require("./routes/login");
+const StudentRouter = require("./routes/student");
+const TeacherRouter = require("./routes/Teacher");
+const AddStudentRouter = require("./routes/AddStudent");
+const cors = require("cors");
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("connected to monogo");
+  })
+  .catch((err) => {
+    console.error("error occured in monogo connection", err);
+  });
 
-app.listen(5000,()=>{console.log('Lisenting...');
-})
+app.use(cors());
+app.use(express.json());
+app.use("/", LoginRouter);
+app.use("/Student", StudentRouter);
+app.use("/Teacher", TeacherRouter);
+app.use("/AddStudent", AddStudentRouter);
+
+app.listen(5000, () => { 
+  console.log("Lisenting...");
+});
+ 
