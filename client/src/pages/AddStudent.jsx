d2b40cfd36c990399849b2d7af56ac3ab7fabbef
R-pradeep2005 <pradeepravisankar1@gmail.com>
2025-07-30 22:44:31 +0530

    Backend: wip alert message in addstudent route

--- client/src/pages/AddStudent.jsx
+++ client/src/pages/AddStudent.jsx
@@ -1,51 +1,73 @@
 import React from "react";
-const handleSubmit = (e) => {
-  e.preventDefault();
-  const data =new FormData(e.target);
-  const student=Object.fromEntries(data.entries());
-  console.log(student);
-  
-  
-
-
-      fetch('http://localhost:5000/AddStudent',{
-        method:'POST',
-        headers:{
-          "content-type":"application/json",
-        },
-        body:JSON.stringify(student)
-
-      })
-}
+import { useState } from "react";
+import Swal from "sweetalert2";
 
 const AddStudent = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const student = Object.fromEntries(data.entries());
+
+    fetch("http://localhost:5000/AddStudent", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(student),
+    })
+      .then((response) => {
+        response.json();
+      })
+      .then((res) => {
+        console.log(res);
+        
+        res.addstudent? window.alert('added sucessfully') : null;
+      });
+  };
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="font-bold text-[24px]">Add new Student</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col items-start gap-2 mt-6">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-start gap-2 mt-6"
+      >
         <label htmlFor="">Name</label>
-        <input name="name" type="text"
+        <input
+          name="name"
+          type="text"
           className="border-1 border-black rounded-xl w-fit p-2"
           placeholder="enter name of the student"
         />
         <label htmlFor="">Email</label>
-        <input name="email" type="email"
+        <input
+          name="email"
+          type="email"
           className="border-1 border-black rounded-xl w-fit p-2"
           placeholder="example@email.com"
         />
 
         <label htmlFor="">Phone No</label>
-        <input name="phone_no" type="text"
+        <input
+          name="phone_no"
+          type="text"
           className="border-1 border-black rounded-xl w-fit p-2"
           placeholder="+91 ..."
         />
-        
+
         <label htmlFor="">Password</label>
-        <input name="password" type="text"
+        <input
+          name="password"
+          type="text"
           className="border-1 border-black rounded-xl w-fit p-2"
           placeholder="enter a password"
         />
-         <button className="bg-blue-600 text-white rounded-xl p-2 font-bold w-full mt-4" type="submit">Submit</button>
+        <button
+          className="bg-blue-600 text-white rounded-xl p-2 font-bold w-full mt-4"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </div>
   );
--- server/routes/AddStudent.js
+++ server/routes/AddStudent.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const router = express.Router();
-const student=require('../model/student')
-
-
-router.post("/", (req, res) => {
+const student = require("../model/student");
+const counter = require("../model/counter");
+async function getStudent_id(student_id) {
+  const data = await counter.findOneAndUpdate(
+    { sequence_id: student_id },
+    { $inc: { sequence_value: 1 } },
+    { new: true, upsert: true }
+  );
+  return data.sequence_value;
+}
+router.post("/", async(req, res) => {
+  const getid=await getStudent_id('studentid');
+  req.body.id=`std_${getid}`
   console.log(req.body);
-  
-  const data=new student(req.body)
-  data.save().then(()=>{console.log('sucessfully inserted');
-  }).catch((err)=>{console.error('error occured in add student data handling',err);
-  })
-   
+  const data = new student(req.body);
+  data
+    .save()
+    .then(() => {
+      res.send(true);
+    })
+    .catch((err) => {
+      console.error("error occured in add student data handling", err);
+    });
 });
 
 module.exports = router;
